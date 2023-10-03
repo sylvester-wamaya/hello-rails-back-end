bbb7945281d556a7a711dd61d1708820f61e64de
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-10-04 00:05:48 +0300

    Fix linter errors

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,9 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: %i[ show update destroy ]
+  before_action :set_message, only: %i[show update destroy]
 
   # GET /messages
   def index
-    @messages = Message.order("RANDOM()").first
+    @messages = Message.order('RANDOM()').first
 
     render json: @messages
   end
